61bf7d5fc52198808d6924bf2b9c139f3c81b53f
naeemalomari <naeemalomari96@gmail.com>
2021-10-02 12:42:01 +0300

    changes

--- app/src/test/java/bitmap/transformer/AppTest.java
+++ app/src/test/java/bitmap/transformer/AppTest.java
@@ -3,11 +3,14 @@
  */
 //package bitmap.transformer;
 
+
+package bitmap.transformer;
 import org.junit.jupiter.api.Test;
-//
-//class AppTest {
-//    @Test void appHasAGreeting() {
-//        App classUnderTest = new App();
-//        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-//    }
-//}
+
+class AppTest {
+    @Test void grayScale() {
+        App test = new App();
+
+
+    }
+}
