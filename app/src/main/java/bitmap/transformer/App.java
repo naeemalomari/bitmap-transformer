16067f8c63cd15fc7864548a06dad2b251080e9d
Mohammad Al-khaleel <83532142+malkhaleel88@users.noreply.github.com>
2021-10-05 15:27:39 +0300

    modify

--- app/src/main/java/bitmap/transformer/App.java
+++ app/src/main/java/bitmap/transformer/App.java
@@ -2,42 +2,30 @@ package bitmap.transformer;
 
 public class App {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
 
         Bitmap bitmap1 = new Bitmap("./app/src/main/resources/Input/inputImage.bmp","./app/src/main/resources/Output/", "first.bmp");
-=======
-        Bitmap bitmap1 = new Bitmap("C:\\Users\\Student\\401Java\\bitmap-transformer\\app\\src\\main\\resources\\Input\\inputImage.bmp","C:\\Users\\Student\\401Java\\bitmap-transformer\\app\\src\\main\\resources\\Output\\", "first.bmp");
->>>>>>> 287d6dc2e7a41fe04f8756ab5e821edad5d22a3d
 
         bitmap1.readFile();
         bitmap1.grayScale();
         bitmap1.saveFile();
 
-<<<<<<< HEAD
+
         Bitmap bitmap2 = new Bitmap("./app/src/main/resources/Input/inputImage.bmp","./app/src/main/resources/Output/", "second.bmp");
-=======
-        Bitmap bitmap2 = new Bitmap("C:\\Users\\Student\\401Java\\bitmap-transformer\\app\\src\\main\\resources\\Input\\inputImage.bmp","C:\\Users\\Student\\401Java\\bitmap-transformer\\app\\src\\main\\resources\\Output\\", "second.bmp");
->>>>>>> 287d6dc2e7a41fe04f8756ab5e821edad5d22a3d
 
         bitmap2.readFile();
         bitmap2.imageFlipHorizontal();
         bitmap2.saveFile();
 
 
-<<<<<<< HEAD
+
         Bitmap bitmap3 = new Bitmap("./app/src/main/resources/Input/inputImage.bmp","./app/src/main/resources/Output/", "third.bmp");
-=======
-        Bitmap bitmap3 = new Bitmap("C:\\Users\\Student\\401Java\\bitmap-transformer\\app\\src\\main\\resources\\Input\\inputImage.bmp","C:\\Users\\Student\\401Java\\bitmap-transformer\\app\\src\\main\\resources\\Output\\", "third.bmp");
->>>>>>> 287d6dc2e7a41fe04f8756ab5e821edad5d22a3d
 
         bitmap3.readFile();
         bitmap3.imageFlipVertical();
         bitmap3.saveFile();
 
 
-<<<<<<< HEAD
     }
-=======
-        }
->>>>>>> 287d6dc2e7a41fe04f8756ab5e821edad5d22a3d
+
 }
