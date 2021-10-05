bd5edcff751bfe5777bf0dda569fb7f267c795a2
Mohammad Al-khaleel <83532142+malkhaleel88@users.noreply.github.com>
2021-10-06 02:40:08 +0300

    Update AppTest.java

--- app/src/test/java/bitmap/transformer/AppTest.java
+++ app/src/test/java/bitmap/transformer/AppTest.java
@@ -12,12 +12,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
      @Test void grayScale() {
-        Bitmap test = new Bitmap("C:\\Users\\Student\\401Java\\bitmap-transformer\\app\\src\\main\\resources\\Input\\inputImage.bmp","C:\\Users\\Student\\401Java\\bitmap-transformer\\app\\src\\main\\resources\\Output\\", "first.bmp");
+        Bitmap test = new Bitmap("./app/src/main/resources/Input/inputImage.bmp","./app/src/main/resources/Onput", "first.bmp");
         assertEquals(true, test.readFile());
 
         Bitmap bitmap = new Bitmap();
-        bitmap.setOutputFilePath("C:\\Users\\Student\\401Java\\bitmap-transformer\\app\\src\\main\\resources\\Output\\");
-        bitmap.setInputFilePath("C:\\Users\\Student\\401Java\\bitmap-transformer\\app\\src\\main\\resources\\Input\\inputImage.bmp");
+        bitmap.setOutputFilePath("./app/src/main/resources/Onput");
+        bitmap.setInputFilePath("./app/src/main/resources/Input/inputImage.bmp");
         bitmap.setNewFileName("first.bmp");
 
         assertEquals(bitmap, test);
